a1c013aa6ad50b36c5edcd02dc1689e56d6d395e
Truong Nhut Tan <nhuttan2111@gmail.com>
2023-02-24 13:22:14 +0700

    TASKDINO-2049: WebCMS fix bug ui home page

--- lib/page/main_page/components/dashboard/dashboard_screen.dart
+++ lib/page/main_page/components/dashboard/dashboard_screen.dart
@@ -1,6 +1,7 @@
 import 'package:admin/component/recent_files.dart';
 import 'package:admin/responsive/responsive.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 import '../../../../component/header.dart';
 import '../../../../component/my_fields.dart';
@@ -13,7 +14,6 @@ class DashboardScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: SingleChildScrollView(
-        primary: false,
         padding: EdgeInsets.all(defaultPadding),
         child: Column(
           children: [
@@ -23,28 +23,26 @@ class DashboardScreen extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Expanded(
-                  flex: 5,
-                  child: Column(
-                    children: [
-                      MyFiles(),
-                      SizedBox(height: defaultPadding),
-                      RecentFiles(),
-                      if (Responsive.isMobile(context))
+                  flex: 4,
+                  child: SizedBox(
+                    height:Responsive.isMobile(context)? Get.height*1.8:Get.height*1,
+                    child: Column(
+                      children: [
+                        MyFiles(),
                         SizedBox(height: defaultPadding),
-                      if (Responsive.isMobile(context)) StarageDetails(),
-                    ],
+                        RecentFiles(),
+                        if (Responsive.isMobile(context))
+                          SizedBox(height: defaultPadding),
+                        if (Responsive.isMobile(context)) Expanded(child: StorageDetails()),
+                      ],
+                    ),
                   ),
                 ),
-                if (!Responsive.isMobile(context))
-                  SizedBox(width: defaultPadding),
-                // On Mobile means if the screen is less than 850 we dont want to show it
-                if (!Responsive.isMobile(context))
-                  Expanded(
-                    flex: 2,
-                    child: StarageDetails(),
-                  ),
+                SizedBox(width: defaultPadding),
+                if (!Responsive.isMobile(context))  Expanded(
+                  flex: 1,child: StorageDetails())
               ],
-            )
+            ),
           ],
         ),
       ),
